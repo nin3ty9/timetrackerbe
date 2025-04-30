e9976d794ebb5d5aef9c0876714fb18e7854c6be
nin3ty9 <user.email>
2025-04-30 15:57:52 +0200

    First commit

---
+++ src/main/java/com/timetrackerbe/timetrackerbe/TimetrackerbeApplication.java
@@ -0,0 +1,13 @@
+package com.timetrackerbe.timetrackerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetrackerbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetrackerbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timetrackerbe/timetrackerbe/TimetrackerbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timetrackerbe.timetrackerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
